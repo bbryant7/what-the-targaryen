492bf3fe2b4a3f3f71db176f3fd90e1c159e577b
saraharless <sara.e.harless@gmail.com>
2017-08-28 16:51:29 -0500

    :sparkles: inserted random generator to get - need to get it to show up on page next :sparkles:

--- app.js
+++ app.js
@@ -3,23 +3,55 @@ const app = express();
 const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
 
-app.engine('mustache',mustacheExpress());
-app.set('views','./views');
-app.set('view engine','mustache');
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
 app.use(bodyParser.urlencoded({
-  extended:false
+  extended: false
 }))
 
-
-let charOptions = [
-  {name: 'Jon Snow',
-  imgsource: './images/jon.gif',
-  isalive: 'alive'
-},
-
+let content = {
+ charOptions: [{
+    name: 'Jon',
+    // imgsource: './images/jon.gif',
+    // isalive: 'alive'
+  },
+  {
+    name: 'Arya',
+    // imgsource: './images/arya.gif',
+    // isalive: 'alive'
+  },
+  {
+    name: 'Tyrion',
+    // isalive: 'alive'
+  },
+  {
+    name: 'Daenerys',
+    // isalive: 'alive'
+  },
+  {
+    name: 'Sansa',
+    // isalive: 'alive'
+  },
+  {
+    name: 'Joffrey',
+    // isalive: 'dead af'
+  },
+  {
+    name: 'Jamie',
+    // isalive: 'alive'
+  }
 ]
+}
 
 
+console.log(content.charOptions[Math.floor(Math.random() * content.charOptions.length)].name);
+
+
+app.get('/', function(req, res) {
+  let round = content.charOptions[Math.floor(Math.random() * content.charOptions.length)].name
+  res.render('game', content);
+})
 
 app.listen(3000, function() {
   console.log('you did it, or something');
